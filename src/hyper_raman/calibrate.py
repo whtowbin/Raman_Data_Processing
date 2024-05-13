21f73a48d62e2313cde6928fe532671a7b81c5f5
Henry Towbin <24940778+whtowbin@users.noreply.github.com>
2024-05-13 10:58:26 -0400

    Attempting to write funtion to interpoalte data to common wavenumber grid. Currently numpy.apply_along_axis() isnt working to iterate along both arrays. I will likely try a different iterator to loop over both.

--- src/hyper_raman/calibrate.py
+++ src/hyper_raman/calibrate.py
@@ -5,39 +5,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 from scipy import optimize
+from scipy import interpolate
 from copy import deepcopy
 
 
-
-
 #%%
-
 calibrate = io.imread("/Users/wtowbin/Projects/hyper-raman/Dev_Data/Unsaturated Raman/Unsaturated Raman 10x_0000.tif")
-
 
 def get_metadata(directory):
     # function to read metadata file for image calibration
     pass
 
 # Running List of image Transformations to apply to full dataset e.g., median filter, normalization, cropping, etc.
-# I can potentially have another list of operations performed on calibration images
 image_transform_log = []
+# I can potentially have another list of operations performed on calibration images
 
 def reset_transform_log(transform_log = image_transform_log):
     transform_log.clear()
 
-# Maybe have each entry in the form (function,{**kwargs})
-# Then I can have another function that applies the log sequentially
-# I will need to have it act on an image object or stack.  
-
+# I should consider adding a way to retaining specifc outputs like dark and bright corrections
 def log_transforms(func, transfrom_log = image_transform_log):
     def wrapper(*args, **kwargs): # args should just be image
         result = func(*args, **kwargs)
         transfrom_log.append((func,kwargs,{"shape":result.shape})) # leave out args since it should just be the image file
         return result
     return wrapper
+#%%
 
-# I could also have another function that keeps track of the array shape after each trans
+def round_half(number):
+    return np.round(number*2)/2
 #%%
 median_calibrate = skfilter.median(calibrate)
 
@@ -45,14 +41,11 @@ median_calibrate = skfilter.median(calibrate)
 # plt.plot(median_calibrate.argmax(axis=0), marker = ".", linestyle = 'none')
 # plt.ylim(219,226)
 
-
 # fig, ax = plt.subplots()
 # ax.imshow(median_calibrate)
 # #plt.ylim(200, 250)
 
 #%%
-
-
 def gauss(x, mu, sd, A=1):
 
     """
@@ -96,7 +89,7 @@ def fit_spectrum_peak(array):
         popt = np.array([np.nan,np.nan,np.nan])
     return popt[0]
 
-#mu, sd, A
+
 def test_fit_spectrum(image):
     indices = index_array(image)
     mid_idx = int(np.round(image.shape[1]/2))
@@ -104,7 +97,6 @@ def test_fit_spectrum(image):
     peak_pos = fit_spectrum_peak(array, indices)
     assert peak_pos is not np.nan
     return peak_pos
-
 
 def nm_to_wn(nm, laser_nm = 532):
    return 1e7* (1/laser_nm - 1/nm)
@@ -115,14 +107,17 @@ def wn_to_nm(wn, laser_nm= 532):
 def pixel_to_nm(peak_pos_nm, peak_pos_pixel, indices, nm_per_pixel = 0.147750):
     return peak_pos_nm + nm_per_pixel * (indices - peak_pos_pixel)
 
-def calibrate_image_wn(array ,peak_pos_wn=1332, laser_nm = 532, nm_per_pixel = 0.147750):
-    """Function for fitting a gaussian peak to an array of 1D Raman unsaturated spectra
-
+def calibrate_array_wn(array ,peak_pos_wn=1332.0, laser_nm = 532, nm_per_pixel = 0.147750):
+    """Function for fitting a gaussian peak to an array of 1D Raman unsaturated spectra and returning an array in wavenumber (cm^-1)
+    The defaults are for the primary diamond Raman peak at 1332 cm^-1
     Args:
-        array (numpy array): 1D numpy array of single unsaturated raman peak
-    
+        array (_type_): 1D numpy array of single unsaturated raman peak
+        peak_pos_wn (float, optional): Reference Peak Location. Defaults to 1332.0 for diamond.
+        laser_nm (int, optional): Excitation laser wavelength. Defaults to 532nm.
+        nm_per_pixel (float, optional): Linear spacing in nanometeres for each pixel. Defaults to 0.147750nm.
+
     Returns:
-        gaussian_max (numpy array): peak position with subpixel accuracy
+        array: 1D array whose values correspond to the wavenumber coordinates for the input array. 
     """
     indices = np.arange(len(array))
     peak_position_init = array.argmax(axis = 0)
@@ -141,23 +136,11 @@ def calibrate_image_wn(array ,peak_pos_wn=1332, laser_nm = 532, nm_per_pixel = 0
     array_wn = nm_to_wn(array_nm, laser_nm)
     return array_wn
 
-
 #%%
-def nm_to_wn(nm, laser_nm = 532):
-   return 1e7* (1/laser_nm - 1/nm)
-
-def wn_to_nm(wn, laser_nm= 532):
-    return 1/(1/laser_nm - wn/10**7)
-
-def pixel_to_nm(peak_pos_nm, peak_pos_pixel, indices, nm_per_pixel = 0.147750):
-    return peak_pos_nm + nm_per_pixel * (indices - peak_pos_pixel)
-    
 
 # Replace Defaults with image metadata pulled when stack is read in
 def calibrate_image(image, peak_pos_wn=1332, laser_nm = 532, nm_per_pixel = 0.147750):
-    #peak_positions = np.apply_along_axis(fit_spectrum_peak, axis = 0,arr = image) 
-    peak_positions = np.apply_along_axis(calibrate_image_wn, axis = 0,arr = image) 
-
+    peak_positions = np.apply_along_axis(calibrate_array_wn, axis = 0,arr = image) 
     return peak_positions
 
 def replace_bad(array_1D, threshold = 0.01):
@@ -170,10 +153,30 @@ def replace_bad(array_1D, threshold = 0.01):
 #np.argwhere(array_1D == np.nan) # find array location
 
 @log_transforms
-def crop_central_columns(image, crop_percent= .1,):
+def crop_central_columns(image, crop_percent= .05):
     crop_low = int(image.shape[1]*crop_percent)
     crop_high = int(image.shape[1]* (1-crop_percent))
     return image[:,crop_low:crop_high]
+
+
+# I am not exactly sure how to do this interpolation and apply it to the images 
+# I think i need to crop the Data and then Interpolate it to the new array. 
+# It might make the most sense to do this in nanometers or pixel units then switch to wavenumber
+# I think that the way it should go is to apply it to each row, func = interpolate(Detector X, Column_Data)
+# func (output array)
+def interpolate_images(calib_image_wn, image, min=None, max= None):
+    interp_spacing = np.round((np.min(calib_image_wn[-1,:] - calib_image_wn[-2,:])),1)
+    if min == None:
+        min = calib_image_wn[0,:].max()
+    if max == None:
+        max = calib_image_wn[-1,:].min()
+    interp_array_wn = np.arange(min,max,interp_spacing)
+    def interp_func(x, y, interp_array = interp_array_wn):
+        return interpolate.CubicSpline(x,y, interp_array)
+    return np.apply_along_axis(interp_func, axis = 0,arr = calib_image_wn, **{"y":image})
+
+    # Maybe Use 
+    
 
 def padding_or_cropping_function(wn_array):
     # This shoud make a repeatable padding or cropping function the can be applied to each image in the same way that whe wavenumber array is to make wavenumber spacing aligned
@@ -182,6 +185,7 @@ def padding_or_cropping_function(wn_array):
 #%%
 wn_calibration_array = calibrate_image(median_calibrate)
 
+
 #%%
 fig, ax = plt.subplots(figsize=(12,8))
 plt.plot(wn_calibration_array[-2, :], marker = ".")
@@ -189,6 +193,11 @@ ax.set_ylim((3560,3580))
 ax.set_xlabel("Column")
 ax.set_ylabel("Pixel row in wn")
 
+#%%
+plt.imshow(wn_calibration_array)
+
+#%%
+# make array from max to min and map data onto it
 
 # %%
 """
